570d2d10722b6db548dcd04630e904f606e8aa09
Daniel-Alencar <danielalencar746@gmail.com>
2024-10-06 00:52:44 -0300

    feat: navigation

--- src/pages/StoryPage/index.jsx
+++ src/pages/StoryPage/index.jsx
@@ -12,31 +12,64 @@ export default function StoryPage() {
     : 0;
   const [storyIndex, setStoryIndex] = useState(savedIndex);
 
+  // Carrega o índice do capítulo do localStorage (ou usa 0 se não houver)
+  const savedChapter = localStorage.getItem('storyChapter') 
+    ? parseInt(localStorage.getItem('storyChapter')) 
+    : 0;
+  const [storyChapter, setStoryChapter] = useState(savedChapter);
+
   // Função para atualizar o índice e salvar no localStorage
   const changeBackStoryIndex = () => {
-    const newIndex = storyIndex - 1;
+    let newIndex = storyIndex - 1;
+
     if (newIndex >= 0) {
       setStoryIndex(newIndex);
       // Salva o progresso no localStorage
       localStorage.setItem('storyIndex', newIndex); 
+    } else {
+      let newChapter = storyChapter - 1;
+      newIndex = story[storyChapter - 1].conversations.length - 1;
+
+      if(newChapter >= 0) {
+        setStoryIndex(newIndex);
+        setStoryChapter(newChapter);
+        // Salva o progresso no localStorage
+        localStorage.setItem('storyIndex', newIndex); 
+        localStorage.setItem('storyChapter', newChapter); 
+      }
     }
   };
 
   // Função para atualizar o índice e salvar no localStorage
   const changeNextStoryIndex = () => {
-    const newIndex = storyIndex + 1;
-    if (newIndex < story.length) {
+    let newIndex = storyIndex + 1;
+    if (newIndex < story[storyChapter].conversations.length) {
       setStoryIndex(newIndex);
       // Salva o progresso no localStorage
       localStorage.setItem('storyIndex', newIndex); 
+    } else {
+      newIndex = 0;
+      let newChapter = storyChapter + 1;
+
+      if(newChapter < story.length) {
+        setStoryIndex(newIndex);
+        setStoryChapter(newChapter);
+        // Salva o progresso no localStorage
+        localStorage.setItem('storyIndex', newIndex); 
+        localStorage.setItem('storyChapter', newChapter); 
+      }
     }
   };
 
   // Função para carregar o progresso ao iniciar a página
   useEffect(() => {
     const storedIndex = localStorage.getItem('storyIndex');
+    const storedChapter = localStorage.getItem('storyChapter');
     if (storedIndex) {
       setStoryIndex(parseInt(storedIndex));
+    }
+    if (storedChapter) {
+      setStoryChapter(parseInt(storedChapter));
     }
   }, []);
 
@@ -46,7 +79,9 @@ export default function StoryPage() {
         className={styles.background} 
         style={{ 
           // Define a imagem de fundo dinamicamente
-          backgroundImage: `url(${story[storyIndex].backgroundImage})`, 
+          backgroundImage: `url(
+            ${story[storyChapter].conversations[storyIndex].backgroundImage}
+          )`, 
           backgroundSize: 'cover',
           height: '100%',
           display: 'flex',
@@ -58,15 +93,15 @@ export default function StoryPage() {
         <div className={styles.character}>
           <img 
             // Define a imagem do personagem dinamicamente
-            src={story[storyIndex].characterImage}
+            src={story[storyChapter].conversations[storyIndex].characterImage}
             alt="Character" 
             className={styles.characterImg}
           />
         </div>
         
         <DialogueBox 
-          title={story[storyIndex].title}
-          text={story[storyIndex].description}
+          title={story[storyChapter].conversations[storyIndex].title}
+          text={story[storyChapter].conversations[storyIndex].description}
         />
       </div>
 
--- src/utils/story.js
+++ src/utils/story.js
@@ -4,65 +4,143 @@ import backgroundImage1 from '../assets/StoryPage/background1.webp';
 import character0 from '../assets/StoryPage/character0.png';
 import character1 from '../assets/StoryPage/character1.png';
 
+// export const story = [
+//   {
+//     title: "Prólogo",
+//     description: "O ano é 2147. A Terra enfrenta tempos difíceis, com recursos se esgotando e a humanidade lutando para sobreviver. Surge um vislumbre de esperança: a descoberta de exoplanetas. Você faz parte da tripulação da nave Stella Explorer, com a missão de encontrar um novo lar para a humanidade.",
+//     backgroundImage: backgroundImage0,
+//     characterImage: character0,
+//   },
+//   {
+//     title: "Capítulo 1: A Decisão",
+//     description: `A sala de comando está silenciosa enquanto a Capitã Aurora explica a missão.
+//     - Capitã Aurora: "A Terra está morrendo. Nossa única esperança é encontrar um planeta habitável. Vamos começar escolhendo nosso primeiro sistema estelar para explorar."
+//     Você é o oficial de ciências e precisa tomar a primeira grande decisão:
+//     - Opção 1: Um sistema com uma estrela semelhante ao Sol.
+//     - Opção 2: Um sistema próximo com uma estrela anã vermelha, mas misterioso.`,
+//     backgroundImage: backgroundImage1,
+//     characterImage: character1,
+//   },
+//   {
+//     title: "Capítulo 2: O Primeiro Contato",
+//     description: `Ao se aproximar do sistema escolhido, a equipe detecta variações no brilho da estrela.
+//     - Dr. Orion: "Parece um trânsito planetário! Pode ser o sinal de um exoplaneta."
+//     Você analisa os dados de luminosidade e descobre um gigante gasoso.
+//     - Capitã Aurora: "Não é habitável, mas é um bom começo. Vamos continuar."
+//     A missão ainda está em seu início, mas o tempo é curto.`,
+//     backgroundImage: backgroundImage0,
+//     characterImage: character0,
+//   },
+//   {
+//     title: "Capítulo 3: O Segredo Oculto",
+//     description: `No próximo sistema, os sensores captam um fenômeno incomum.
+//     - Dr. Orion: "A estrela está oscilando! Isso pode ser causado por um planeta massivo."
+//     Ao investigar, você descobre um exoplaneta com sinais de água e uma atmosfera densa.
+//     - Capitã Aurora: "Este pode ser o nosso novo lar, mas precisamos de mais dados."
+//     A equipe se prepara para explorar mais profundamente.`,
+//     backgroundImage: backgroundImage1,
+//     characterImage: character1,
+//   },
+//   {
+//     title: "Capítulo 4: Explorando Novos Mundos",
+//     description: `Após meses de viagem, a Stella Explorer chega ao exoplaneta promissor. O pouso é suave, mas o planeta é instável.
+//     - Capitã Aurora: "Preparem-se! Tempestades se aproximam!"
+//     Você precisa coletar dados sobre o clima e recursos do planeta antes que a situação piore. Decisão importante:
+//     - Evacuar o planeta rapidamente.
+//     - Continuar coletando informações, arriscando a segurança da tripulação.`,
+//     backgroundImage: backgroundImage0,
+//     characterImage: character0,
+//   },
+//   {
+//     title: "Capítulo 5: O Dilema Final",
+//     description: `A nave capta um fenômeno de microlente gravitacional.
+//     - Dr. Orion: "Outro planeta pode estar nos esperando. Mas nossos recursos estão no limite."
+//     A tripulação precisa decidir: adaptar o planeta atual para colonização ou arriscar e buscar um novo mundo. O destino da humanidade está em suas mãos.`,
+//     backgroundImage: backgroundImage1,
+//     characterImage: character1,
+//   },
+//   {
+//     title: "Epílogo: O Futuro da Humanidade",
+//     description: "As decisões tomadas moldam o futuro da humanidade. Se bem-sucedido, você verá as colônias crescerem e a expansão da espécie. Caso contrário, a missão falha, e a Terra colapsa, mas o legado da tripulação ecoa nas estrelas.",
+//     backgroundImage: backgroundImage0,
+//     characterImage: character0,
+//   }
+// ];
+
 export const story = [
   {
-    title: "Prólogo",
-    description: "O ano é 2147. A Terra enfrenta tempos difíceis, com recursos se esgotando e a humanidade lutando para sobreviver. Surge um vislumbre de esperança: a descoberta de exoplanetas. Você faz parte da tripulação da nave Stella Explorer, com a missão de encontrar um novo lar para a humanidade.",
-    backgroundImage: backgroundImage0,
-    characterImage: character0,
+    chapter: 0,
+    conversations: [
+      {
+        title: "Prólogo",
+        description: "O ano é 2147. A Terra enfrenta tempos difíceis, com recursos se esgotando e a humanidade lutando para sobreviver. Surge um vislumbre de esperança: a descoberta de exoplanetas. Você faz parte da tripulação da nave Stella Explorer, com a missão de encontrar um novo lar para a humanidade.",
+        backgroundImage: backgroundImage0,
+        characterImage: character0,
+      },
+      {
+        title: "Capítulo 1: A Decisão",
+        description: `A sala de comando está silenciosa enquanto a Capitã Aurora explica a missão.
+        - Capitã Aurora: "A Terra está morrendo. Nossa única esperança é encontrar um planeta habitável. Vamos começar escolhendo nosso primeiro sistema estelar para explorar."
+        Você é o oficial de ciências e precisa tomar a primeira grande decisão:
+        - Opção 1: Um sistema com uma estrela semelhante ao Sol.
+        - Opção 2: Um sistema próximo com uma estrela anã vermelha, mas misterioso.`,
+        backgroundImage: backgroundImage1,
+        characterImage: character1,
+      },
+      {
+        title: "Capítulo 2: O Primeiro Contato",
+        description: `Ao se aproximar do sistema escolhido, a equipe detecta variações no brilho da estrela.
+        - Dr. Orion: "Parece um trânsito planetário! Pode ser o sinal de um exoplaneta."
+        Você analisa os dados de luminosidade e descobre um gigante gasoso.
+        - Capitã Aurora: "Não é habitável, mas é um bom começo. Vamos continuar."
+        A missão ainda está em seu início, mas o tempo é curto.`,
+        backgroundImage: backgroundImage0,
+        characterImage: character0,
+      },
+    ],
   },
   {
-    title: "Capítulo 1: A Decisão",
-    description: `A sala de comando está silenciosa enquanto a Capitã Aurora explica a missão.
-    - Capitã Aurora: "A Terra está morrendo. Nossa única esperança é encontrar um planeta habitável. Vamos começar escolhendo nosso primeiro sistema estelar para explorar."
-    Você é o oficial de ciências e precisa tomar a primeira grande decisão:
-    - Opção 1: Um sistema com uma estrela semelhante ao Sol.
-    - Opção 2: Um sistema próximo com uma estrela anã vermelha, mas misterioso.`,
-    backgroundImage: backgroundImage1,
-    characterImage: character1,
+    chapter: 1,
+    conversations: [
+      {
+        title: "Capítulo 3: O Segredo Oculto",
+        description: `No próximo sistema, os sensores captam um fenômeno incomum.
+        - Dr. Orion: "A estrela está oscilando! Isso pode ser causado por um planeta massivo."
+        Ao investigar, você descobre um exoplaneta com sinais de água e uma atmosfera densa.
+        - Capitã Aurora: "Este pode ser o nosso novo lar, mas precisamos de mais dados."
+        A equipe se prepara para explorar mais profundamente.`,
+        backgroundImage: backgroundImage1,
+        characterImage: character1,
+      },
+      {
+        title: "Capítulo 4: Explorando Novos Mundos",
+        description: `Após meses de viagem, a Stella Explorer chega ao exoplaneta promissor. O pouso é suave, mas o planeta é instável.
+        - Capitã Aurora: "Preparem-se! Tempestades se aproximam!"
+        Você precisa coletar dados sobre o clima e recursos do planeta antes que a situação piore. Decisão importante:
+        - Evacuar o planeta rapidamente.
+        - Continuar coletando informações, arriscando a segurança da tripulação.`,
+        backgroundImage: backgroundImage0,
+        characterImage: character0,
+      },
+      {
+        title: "Capítulo 5: O Dilema Final",
+        description: `A nave capta um fenômeno de microlente gravitacional.
+        - Dr. Orion: "Outro planeta pode estar nos esperando. Mas nossos recursos estão no limite."
+        A tripulação precisa decidir: adaptar o planeta atual para colonização ou arriscar e buscar um novo mundo. O destino da humanidade está em suas mãos.`,
+        backgroundImage: backgroundImage1,
+        characterImage: character1,
+      },
+    ]
   },
   {
-    title: "Capítulo 2: O Primeiro Contato",
-    description: `Ao se aproximar do sistema escolhido, a equipe detecta variações no brilho da estrela.
-    - Dr. Orion: "Parece um trânsito planetário! Pode ser o sinal de um exoplaneta."
-    Você analisa os dados de luminosidade e descobre um gigante gasoso.
-    - Capitã Aurora: "Não é habitável, mas é um bom começo. Vamos continuar."
-    A missão ainda está em seu início, mas o tempo é curto.`,
-    backgroundImage: backgroundImage0,
-    characterImage: character0,
-  },
-  {
-    title: "Capítulo 3: O Segredo Oculto",
-    description: `No próximo sistema, os sensores captam um fenômeno incomum.
-    - Dr. Orion: "A estrela está oscilando! Isso pode ser causado por um planeta massivo."
-    Ao investigar, você descobre um exoplaneta com sinais de água e uma atmosfera densa.
-    - Capitã Aurora: "Este pode ser o nosso novo lar, mas precisamos de mais dados."
-    A equipe se prepara para explorar mais profundamente.`,
-    backgroundImage: backgroundImage1,
-    characterImage: character1,
-  },
-  {
-    title: "Capítulo 4: Explorando Novos Mundos",
-    description: `Após meses de viagem, a Stella Explorer chega ao exoplaneta promissor. O pouso é suave, mas o planeta é instável.
-    - Capitã Aurora: "Preparem-se! Tempestades se aproximam!"
-    Você precisa coletar dados sobre o clima e recursos do planeta antes que a situação piore. Decisão importante:
-    - Evacuar o planeta rapidamente.
-    - Continuar coletando informações, arriscando a segurança da tripulação.`,
-    backgroundImage: backgroundImage0,
-    characterImage: character0,
-  },
-  {
-    title: "Capítulo 5: O Dilema Final",
-    description: `A nave capta um fenômeno de microlente gravitacional.
-    - Dr. Orion: "Outro planeta pode estar nos esperando. Mas nossos recursos estão no limite."
-    A tripulação precisa decidir: adaptar o planeta atual para colonização ou arriscar e buscar um novo mundo. O destino da humanidade está em suas mãos.`,
-    backgroundImage: backgroundImage1,
-    characterImage: character1,
-  },
-  {
-    title: "Epílogo: O Futuro da Humanidade",
-    description: "As decisões tomadas moldam o futuro da humanidade. Se bem-sucedido, você verá as colônias crescerem e a expansão da espécie. Caso contrário, a missão falha, e a Terra colapsa, mas o legado da tripulação ecoa nas estrelas.",
-    backgroundImage: backgroundImage0,
-    characterImage: character0,
+    chapter: 2,
+    conversations: [
+      {
+        title: "Epílogo: O Futuro da Humanidade",
+        description: "As decisões tomadas moldam o futuro da humanidade. Se bem-sucedido, você verá as colônias crescerem e a expansão da espécie. Caso contrário, a missão falha, e a Terra colapsa, mas o legado da tripulação ecoa nas estrelas.",
+        backgroundImage: backgroundImage0,
+        characterImage: character0,
+      }
+    ]
   }
 ];
